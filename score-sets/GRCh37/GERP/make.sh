32ed6ed9833acf846840d069c08f9abea96f5e4c
Jim Havrilla <u1021864@kingspeak23.wasatch.peaks>
2017-08-09 16:07:08 -0600

    fixes to gerp score build

--- score-sets/GRCh37/GERP/make.sh
+++ score-sets/GRCh37/GERP/make.sh
@@ -1,12 +1,15 @@
 set -euo pipefail
 
-wget http://mendel.stanford.edu/SidowLab/downloads/gerp/hg19.GERP_scores.tar.gz
-mkdir gerp
-tar xzvf ../hg19.GERP_scores.tar.gz
+mkdir -p gerp
+cd gerp
+if [ ! -s hg19.GERP_scores.tar.gz ]; then
+    wget http://mendel.stanford.edu/SidowLab/downloads/gerp/hg19.GERP_scores.tar.gz
+    tar xzvf hg19.GERP_scores.tar.gz
+fi
 
 ( echo -e "#chrom\tstart\tgerp_rs"
 for chr in $(seq 1 22) X Y; do
     >&2 echo $chr
     awk -vchr=$chr 'BEGIN{OFS="\t"}{ print chr,NR,$2 }' chr$chr.maf.rates
-done ) | bgzip -@ 2 -c > gerp_rs.txt.gz
-tabix -b2 -e2 gerp_rs.txt.gz
+done ) | bgzip -@ 2 -c > ../gerp_rs.txt.gz
+tabix -b2 -e2 ../gerp_rs.txt.gz
