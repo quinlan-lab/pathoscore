8fd02da991037379e4df60e308f2c9ad2a80ee29
David McGaughey <mcgaughey@gmail.com>
2018-02-26 09:50:42 -0500

    wget to pull DANN tsv file and tabix index. They dont need any processing

---
+++ score-sets/GRCh37/DANN/make.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# can be used as is with pathoscore.py with --scores score-sets/GRCh37/DANN/DANN_whole_genome_SNVs.tsv.bgz:DANN:5:self
+dann_file="DANN_whole_genome_SNVs.tsv.bgz"
+if [ ! -f $dann_file ]; then
+	wget https://cbcl.ics.uci.edu/public_data/DANN/data/DANN_whole_genome_SNVs.tsv.bgz
+	wget https://cbcl.ics.uci.edu/public_data/DANN/data/DANN_whole_genome_SNVs.tsv.bgz.tbi
+fi
+
