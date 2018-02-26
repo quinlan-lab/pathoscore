099e155d6d3d6161d4338ca1d0b916c5b37470ed
David McGaughey <mcgaughey@gmail.com>
2018-02-26 09:56:26 -0500

    index wget split apart

--- score-sets/GRCh37/DANN/make.sh
+++ score-sets/GRCh37/DANN/make.sh
@@ -4,6 +4,10 @@
 dann_file="DANN_whole_genome_SNVs.tsv.bgz"
 if [ ! -f $dann_file ]; then
 	wget https://cbcl.ics.uci.edu/public_data/DANN/data/DANN_whole_genome_SNVs.tsv.bgz
+fi
+
+dann_index="DANN_whole_genome_SNVs.tsv.bgz.tbi"
+if [ ! -f $dann_index ]; then
 	wget https://cbcl.ics.uci.edu/public_data/DANN/data/DANN_whole_genome_SNVs.tsv.bgz.tbi
 fi
 
