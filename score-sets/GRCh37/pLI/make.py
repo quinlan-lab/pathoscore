59d39654c983a9d4499bec8b709d1c43fed91b7d
Jim Havrilla <u1021864@kingspeak23.wasatch.peaks>
2018-05-04 14:43:29 -0600

    pLI change, added in gene to file for more information

--- score-sets/GRCh37/pLI/make.py
+++ score-sets/GRCh37/pLI/make.py
@@ -3,8 +3,8 @@ import toolshed as ts
 
 vals = []
 for d in ts.reader(sys.argv[1]):
-    vals.append((d['chr'], int(d['tx_start']), int(d['tx_end']), d['pLI']))
+    vals.append((d['chr'], int(d['tx_start']), int(d['tx_end']), d['gene'], d['pLI']))
 vals.sort()
 for v in vals:
-    print("{chrom}\t{start}\t{end}\t{pLI}".format(chrom=v[0], start=v[1],
-        end=v[2], pLI=v[3]))
+    print("{chrom}\t{start}\t{end}\t{gene}\t{pLI}".format(chrom=v[0], start=v[1],
+        end=v[2], gene=v[3], pLI=v[4]))
