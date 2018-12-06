91aec37fcadca02c22f54128a33c507764107a54
Jim Havrilla <u1021864@kingspeak31.wasatch.peaks>
2018-12-06 11:50:38 -0700

    fixes to metasvm, sift, polyphen (can simply not use cadd score precomputed)

--- score-sets/GRCh37/polyphen2/make.sh
+++ score-sets/GRCh37/polyphen2/make.sh
@@ -1,13 +1,20 @@
-#HumDiv version (5th column)
-
-if [ ! -s Polyphen2_HDIV.sorted.txt.gz ]; then
-    wget https://s3.us-east-2.amazonaws.com/pathoscore-data/precomputed-metrics/Polyphen2_HDIV.sorted.txt.gz
-    wget https://s3.us-east-2.amazonaws.com/pathoscore-data/precomputed-metrics/Polyphen2_HDIV.sorted.txt.gz.tbi
+# hdiv col 5, hvar col 6
+if [ ! -s polyphen-2.2.2-whess-2011_12.tab.tar.bz2 ]; then
+    wget ftp://genetics.bwh.harvard.edu/pph2/whess/polyphen-2.2.2-whess-2011_12.tab.tar.bz2
 fi
-
-#HumVar version (5th column)
-
-if [ ! -s Polyphen2_HVAR.sorted.txt.gz ]; then
-    wget https://s3.us-east-2.amazonaws.com/pathoscore-data/precomputed-metrics/Polyphen2_HVAR.sorted.txt.gz
-    wget https://s3.us-east-2.amazonaws.com/pathoscore-data/precomputed-metrics/Polyphen2_HVAR.sorted.txt.gz.tbi
-fi
+mkdir -p polyphen2
+cd polyphen2
+tar xjvf ../polyphen-2.2.2-whess-2011_12.tab.tar.bz2
+set -euo pipefail
+(echo -e "#chrom\tpos\tref\talt\thdiv\thvar"
+for f in polyphen-2.2.2-whess-2011_12/*features.tab; do
+    set -e
+    s=$(dirname $f)/$(basename $f .features.tab).scores.tab
+    paste $f $s | cut -f 1,2,58,64 
+done | grep -v ^# \
+    | perl -pe 's/:|\//\t/g' \
+    | sed 's/^chr//; s/ //g' \
+    | awk '$4 != ""' \
+    | sort -k1,1 -k2,2n) | bgzip -c > polyphen2.txt.gz
+ tabix -b2 -e2 polyphen2.txt.gz
+cd -
